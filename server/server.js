66d8477dafe121f9dba842c68fca5eda49837238
kazikai <kazikai@github.com>
2017-09-17 22:31:30 +0900

    get todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const { ObjectID } = require('mongodb');
 const { mongoose } = require('./db/mongoose');
 const { Todo } = require('./models/todo');
 const { User } = require('./models/user');
@@ -22,6 +23,7 @@ app.post('/todos', (req, res) => {
   });
 });
 
+//GET /todos/
 app.get('/todos', (req, res) => {
   Todo.find().then((todos) => {
     res.send({
@@ -32,7 +34,25 @@ app.get('/todos', (req, res) => {
   });
 });
 
-//GET /todos/
+//GET /todos/:id
+
+app.get('/todos/:id', (req, res) => {
+  const id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send('not valid id');
+  }
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.satust(404).send();
+    }
+    res.send({
+      todo
+    });
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
 
 
 app.listen(port, () => {
